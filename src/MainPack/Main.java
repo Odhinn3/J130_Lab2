4cf12754cd301cfe1eb0c2a6181ed0af9fd295ac
A. Konnov <101521850+Odhinn3@users.noreply.github.com>
2022-08-28 18:46:51 +0300

    Лабораторная 2. Часть 1. Выполнены все условия лаб. работы. Написан метод регистрации заказа (количество позиций каждого товара задано ОДНОЙ переменной класса Model)

--- src/MainPack/Main.java
+++ src/MainPack/Main.java
@@ -2,6 +2,9 @@
 package MainPack;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  *
@@ -13,12 +16,24 @@ public class Main {
         Repository repo = new Repository();
         repo.getModelList().forEach(System.out::println);
         repo.getModelByID(3);
-        ArrayList<String> articuls = new ArrayList<String>();
+        //создаем динамический массив позиций в заказе
+        //массив и карта очищаются в конце работы метода regOrder
+        ArrayList<String> articuls = new ArrayList<>();
+        Map<String, Integer> orderpos = new HashMap<String, Integer>();
+        //добавляем одну позицию
+        orderpos.clear();
+        articuls.clear();
+        orderpos.put("3261615", 8);
         articuls.add("3251615");
-        repo.regOrder("TEST", "(921)000-00-00", "test@test.com", "TEST", articuls, 8);
+        //регистрируем заказ с одной позицией
+        repo.regOrder("TEST", "(921)000-00-00", "test@test.com", "TEST", articuls, orderpos);
+        //добавляем две позиции
+        orderpos.put("3251616", 10);
         articuls.add("3251616");
+        orderpos.put("3251619", 10);
         articuls.add("3251619");
-        repo.regOrder("TEST", "(921)000-00-00", "test@test.com", "TEST", articuls, 8);
+        //регистрируем заказ с двумя позициями
+        repo.regOrder("TEST", "(921)000-00-00", "test@test.com", "TEST", articuls, orderpos);
         
     }
 }
--- src/MainPack/Repository.java
+++ src/MainPack/Repository.java
@@ -8,9 +8,9 @@ package MainPack;
 import MySQL_DB.Model;
 import java.sql.*;
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -20,11 +20,13 @@ public class Repository {
     public String url = "jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=CONVERT_TO_NULL";
     public String user = "root";
     public String password = "C_1kr2161240";
+    
+
 
     //получение списка продуктов
     public List<Model> getModelList(){
         List<Model> list = new ArrayList<>();
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=CONVERT_TO_NULL", "root", "C_1kr2161240");
+        try (Connection con = DriverManager.getConnection(url, user, password);
                 Statement stm = con.createStatement()){
             ResultSet rs = stm.executeQuery("SELECT * FROM javadev.products");
             while (rs.next()){
@@ -44,7 +46,7 @@ public class Repository {
     //вывод в консоль списка наименований товаров заказов с заданным ID
     public List<Model> getModelByID(int id){
         List<Model> list = new ArrayList<>();
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=CONVERT_TO_NULL", "root", "C_1kr2161240");
+        try (Connection con = DriverManager.getConnection(url, user, password);
                 Statement stm = con.createStatement()){
             ResultSet rs = stm.executeQuery("SELECT javadev.products.name, javadev.products.color\n" +
                 "FROM javadev.products, javadev.orders, javadev.orderpos\n" +
@@ -71,44 +73,51 @@ public class Repository {
         return list;
     }
     //регистрация заказа
-    public void regOrder(String name, String phone, String email, String address, ArrayList<String> articuls, int num) throws SQLException{
+    public void regOrder(String name, String phone, String email, String address, ArrayList<String> articuls, Map<String, Integer> orderpos) throws SQLException{
         if(name == null || phone == null || email == null || address == null ||
-                articuls == null || num == 0){
+                articuls == null){
             throw new IllegalArgumentException("Не заданы аргументы!");
         } 
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=CONVERT_TO_NULL", "root", "C_1kr2161240");
+        try (Connection con = DriverManager.getConnection(url, user, password);
             Statement stm = con.createStatement()){
             if(articuls.size()==1){
+                String a = articuls.get(0);
+                ArrayList<Integer> v = new ArrayList<>(orderpos.values());
+                int n = v.get(0);
                 stm.executeUpdate("INSERT INTO javadev.orders "
-                        + "(id, orderdate, customername, phone, email, address, status, shipdate) VALUES"
-                        + "((SELECT * FROM (SELECT MAX(id) FROM javadev.orders) AS t)+1, '"
-                        + LocalDate.now() + "', \""
-                        + name + "\", \"" + phone + "\", \"" + email + "\", \"" + address
-                        + "\", \"S"  + "\", '" + LocalDate.now() + "')");
+                    + "(id, orderdate, customername, phone, email, address, status, shipdate) VALUES"
+                    + "((SELECT * FROM (SELECT MAX(id) FROM javadev.orders) AS t)+1, '"
+                    + LocalDate.now() + "', \""
+                    + name + "\", \"" + phone + "\", \"" + email + "\", \"" + address
+                    + "\", \"P"  + "\", null)");
+
                 stm.executeUpdate("INSERT INTO javadev.orderpos (ordercode, articul, price, num)\n" +
-                    "VALUES ((SELECT MAX(id) FROM javadev.orders), \"" + articuls.get(0)
-                        + "\", (SELECT price FROM javadev.products WHERE javadev.products.articul=\""
-                        + articuls.get(0) + "\"), " + num + ")");
+                    "VALUES ((SELECT MAX(id) FROM javadev.orders), \"" + a
+                    + "\", (SELECT price FROM javadev.products WHERE javadev.products.articul=\""
+                    + a + "\"), " + n + ")");
             } else {
                 stm.executeUpdate("INSERT INTO javadev.orders "
                     + "(id, orderdate, customername, phone, email, address, status, shipdate) VALUES"
                     + "((SELECT * FROM (SELECT MAX(id) FROM javadev.orders) AS t)+1, '"
                     + LocalDate.now() + "', \""
                     + name + "\", \"" + phone + "\", \"" + email + "\", \"" + address
-                    + "\", \"S"  + "\", '" + LocalDate.now() + "')");
+                    + "\", \"P"  + "\", null)");
                 ArrayList<String> sec = new ArrayList<>();
                 sec.addAll(articuls);
                 for (String i : sec){
                     String a = i;
+                    int n = orderpos.get(a);
                         stm.executeUpdate("INSERT INTO javadev.orderpos (ordercode, articul, price, num)\n" +
                             "VALUES ((SELECT MAX(id) FROM javadev.orders), \"" + a
                             + "\", (SELECT price FROM javadev.products WHERE javadev.products.articul=\""
-                            + a + "\"), " + num + ")");
+                            + a + "\"), " + n + ")");
                 }
             }
         } catch (SQLException ex) {
         ex.printStackTrace();
         }
         articuls.clear();
+        orderpos.clear();
+
     }
 }
